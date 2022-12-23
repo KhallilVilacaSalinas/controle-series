31349e59120d3c7982480b615a691edb71dc2d40
Vilaca <khallil.vilaca@gmail.com>
2022-12-23 09:28:45 -0300

    creating and listing series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+        $query = DB::select('SELECT nome FROM series');
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')->with('series', $query);
     }
 
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nameSerie = $request->input('nome');
+
+        $query = DB::insert('INSERT INTO series (nome) VALUES (?)', [$nameSerie]);
+
+        if ($query) {
+            return 'OK';
+        } else {
+            return 'Deu erro';
+        }
     }
 }
